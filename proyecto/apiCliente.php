4eefdc9295be5ef7ca0574c9a5a55ccb0f32ab3e
RICO LÓPEZ <diericlop@alu.edu.gva.es>
2023-12-19 19:27:19 +0100

    Acabado: Clientes

--- proyecto/apiCliente.php
+++ proyecto/apiCliente.php
@@ -20,28 +20,87 @@ switch ($_SERVER["REQUEST_METHOD"]) {
         break;
 }
 
-function getClientes($conex){
-    $sql = "SELECT * FROM clientes;";
+function getClientes($conex)
+{
+    $input = json_decode(file_get_contents('php://input'), true);
 
-    $result = $conex->query($sql);
+    $usuario = $input['usuario'];
 
-    $clientes = array();
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)){
-        $clientes[] = $row;
+    if (($adminStatus = getAdminStatusUser($conex, $usuario)) == false) {
+        echo json_encode(array("error" => "Error al obtener el estado de administrador del usuario"));
+        return;
+    } else {
+        if ($adminStatus["admin"] == 1) {
+            $sql = "SELECT * FROM clientes;";
+
+            $result = $conex->query($sql);
+
+            $clientes = array();
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                $clientes[] = $row;
+            }
+            echo json_encode($clientes);
+        } else {
+            echo json_encode(array("error" => "El usuario no es administrador"));
+        }
     }
-    echo json_encode($clientes);
 
 }
 
-function insertCliente($conex){
+function insertCliente($conex)
+{
     $input = json_decode(file_get_contents('php://input'), true);
-    
+
     $cliente = $input["cliente"];
 
     $sql = "INSERT INTO clientes (nombre, apellidos, telefono, email) VALUES (?,?,?,?);";
     $result = $conex->query($sql);
     $result->execute([$cliente["nombre"], $cliente["apellidos"], $cliente["telefono"], $cliente["email"]]);
     echo json_encode($result);
+}
+
+function updateCliente($conex)
+{
+    $input = json_decode(file_get_contents('php://input'), true);
+
+    $usuario = $input['usuario'];
+    $cliente = $input["cliente"];
+
+    if(($adminStatus = getAdminStatusUser($conex, $usuario)) == false){
+        echo json_encode(array("error" => "Error al obtener el estado de administrador del usuario"));
+        return;
+    } else {
+        if($adminStatus["admin"] == 1){
+            $sql = "UPDATE clientes SET nombre = ?, apellidos = ?, telefono = ?, email = ? WHERE id = ?;";
+            $result = $conex->query($sql);
+            $result->execute([$cliente["nombre"], $cliente["apellidos"], $cliente["telefono"], $cliente["email"], $cliente["id"]]);
+            echo json_encode($result);
+        } else {
+            echo json_encode(array("error" => "El usuario no es administrador"));
+        }
+    }
+}
+
+function deleteCliente($conex)
+{
+    $input = json_decode(file_get_contents('php://input'), true);
+
+    $usuario = $input['usuario'];
+    $cliente = $input["cliente"];
+
+    if(($adminStatus = getAdminStatusUser($conex, $usuario)) == false){
+        echo json_encode(array("error" => "Error al obtener el estado de administrador del usuario"));
+        return;
+    } else {
+        if($adminStatus["admin"] == 1){
+            $sql = "DELETE FROM clientes WHERE id = ?;";
+            $result = $conex->query($sql);
+            $result->execute([$cliente["id"]]);
+            echo json_encode($result);
+        } else {
+            echo json_encode(array("error" => "El usuario no es administrador"));
+        }
+    }
 }
 
 
--- proyecto/conexion.php
+++ proyecto/conexion.php
@@ -9,4 +9,11 @@ function conectarBd(){
     }
 }
 
+function getAdminStatusUser($conex, $usuario){
+    $sql = "SELECT admin FROM usuarios WHERE id = ". $usuario["id"] . ";";
+    $result = $conex->query($sql);
+    $adminStatusUser = $result->fetch(PDO::FETCH_ASSOC);
+    return $adminStatusUser;
+}
+
 ?>
